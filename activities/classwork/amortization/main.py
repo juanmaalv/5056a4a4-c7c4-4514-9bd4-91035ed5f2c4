a35da82067d4cf76a6a0996278823757b5b47a3f
rhdzmota <rhdzmota@gmail.com>
2019-10-02 18:04:47 -0500

    Class Solution

--- activities/classwork/amortization/main.py
+++ activities/classwork/amortization/main.py
@@ -11,14 +11,23 @@ class Main(object):
 
     @staticmethod
     def annuity(amount, interest, n):
-        pass
+        amortization = Amortization(amount, interest, n)
+        return amortization.annuity
 
-    def table(self, amount, interest, n, rows=10, save=""):
-        pass
+    def table(self, amount, interest, n, save=""):
+        amortization = Amortization(amount, interest, n)
+        df = amortization.get_table()
+        print(df)
+        if save:
+            df.to_csv(save)
 
     def graph(self, amount, interest, n, save=""):
-        pass
+        amortization = Amortization(amount, interest, n)
+        fig = amortization.get_plot()
+        if save:
+            fig.savefig(save)
 
 
 if __name__ == "__main__":
     fire.Fire(Main)
+
